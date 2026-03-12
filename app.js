d5d423adb948b245a086f566687da468e3357a8f
itz-nitant25 <nitant.amb252011@gmail.com>
2026-03-12 14:12:15 +0530

    Refactor app.js for improved readability and structure

--- app.js
+++ app.js
@@ -1,32 +1,45 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 
 import {
+
 getAuth,
+
 GoogleAuthProvider,
-signInWithPopup
+
+signInWithPopup,
+
+signOut,
+
+onAuthStateChanged
+
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 import {
+
 getFirestore,
+
 collection,
+
 addDoc,
+
 getDocs
+
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 
 const firebaseConfig = {
 
-apiKey: "AIzaSyBtotVbhe6RBn2QPQCSXbCVcY9rL_36KwM",
+apiKey:"AIzaSyBtotVbhe6RBn2QPQCSXbCVcY9rL_36KwM",
 
-authDomain: "toppertimer-70de2.firebaseapp.com",
+authDomain:"toppertimer-70de2.firebaseapp.com",
 
-projectId: "toppertimer-70de2",
+projectId:"toppertimer-70de2",
 
-storageBucket: "toppertimer-70de2.appspot.com",
+storageBucket:"toppertimer-70de2.appspot.com",
 
-messagingSenderId: "809874833955",
+messagingSenderId:"809874833955",
 
-appId: "1:809874833955:web:55580be4fcb2534a822796"
+appId:"1:809874833955:web:55580be4fcb2534a822796"
 
 };
 
@@ -40,35 +53,73 @@ const db = getFirestore(app);
 const provider = new GoogleAuthProvider();
 
 
-let user = null;
+let seconds=0;
 
-let seconds = 0;
-
-let interval = null;
+let interval=null;
 
 
-document.getElementById("loginBtn").onclick = async () => {
+const userName=document.getElementById("userName");
 
-const result = await signInWithPopup(auth, provider);
+const profilePic=document.getElementById("profilePic");
 
-user = result.user;
 
-alert("Welcome " + user.displayName);
+document.getElementById("loginBtn").onclick=()=>{
+
+signInWithPopup(auth,provider);
 
 };
 
 
-document.getElementById("startBtn").onclick = () => {
+document.getElementById("signupBtn").onclick=()=>{
 
-interval = setInterval(() => {
+signInWithPopup(auth,provider);
+
+};
+
+
+onAuthStateChanged(auth,(user)=>{
+
+if(user){
+
+userName.innerText=user.displayName;
+
+profilePic.src=user.photoURL;
+
+}
+
+});
+
+
+document.getElementById("settingsBtn").onclick=()=>{
+
+let menu=document.getElementById("settingsMenu");
+
+menu.style.display=menu.style.display==="block"?"none":"block";
+
+};
+
+
+document.getElementById("logoutBtn").onclick=()=>{
+
+signOut(auth);
+
+location.reload();
+
+};
+
+
+document.getElementById("startBtn").onclick=()=>{
+
+interval=setInterval(()=>{
 
 seconds++;
 
-let min = Math.floor(seconds / 60);
+let min=Math.floor(seconds/60);
 
-let sec = seconds % 60;
+let sec=seconds%60;
 
-document.getElementById("time").innerText =
+document.getElementById("time").innerText=
+
 `${min}:${sec.toString().padStart(2,"0")}`;
 
 },1000);
@@ -76,9 +127,11 @@ document.getElementById("time").innerText =
 };
 
 
-document.getElementById("stopBtn").onclick = async () => {
+document.getElementById("stopBtn").onclick=async()=>{
 
 clearInterval(interval);
+
+const user=auth.currentUser;
 
 if(user){
 
@@ -94,26 +147,33 @@ loadLeaderboard();
 
 }
 
-seconds = 0;
+};
+
+
+document.getElementById("resetBtn").onclick=()=>{
+
+seconds=0;
+
+document.getElementById("time").innerText="00:00";
 
 };
 
 
 async function loadLeaderboard(){
 
-const querySnapshot = await getDocs(collection(db,"studyTimes"));
+const querySnapshot=await getDocs(collection(db,"studyTimes"));
 
-const list = document.getElementById("leaderboardList");
+const list=document.getElementById("leaderboardList");
 
-list.innerHTML = "";
+list.innerHTML="";
 
 querySnapshot.forEach(doc=>{
 
-let data = doc.data();
+let data=doc.data();
 
-let li = document.createElement("li");
+let li=document.createElement("li");
 
-li.innerText = data.name + " - " + data.time + " sec";
+li.innerText=data.name+" - "+data.time+" sec";
 
 list.appendChild(li);
 
